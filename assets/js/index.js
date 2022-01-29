bef8da6db0d449bc8d54e780fa27306ab67de064
Anton Cherry <39146381+ztxone@users.noreply.github.com>
2022-01-30 01:02:57 +0200

    actual build with locomotive-scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,27 +4,27 @@ const scroll = new LocomotiveScroll({
   smooth: true,
   repeat: true
 })
-const intro = document.querySelector('.intro')
-const introH1 = document.querySelector('.intro__h1')
+// const intro = document.querySelector('.intro')
+// const introH1 = document.querySelector('.intro__h1')
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
-console.log("Hello");
-sleep(2000).then(() => { console.log("World!"); });
-function fadeOut() {
-  setTimeout(() => {
-    introH1.classList.remove('animate-pulse')
+// console.log("Hello");
+// sleep(2000).then(() => { console.log("World!"); });
+// function fadeOut() {
+//   setTimeout(() => {
+//     introH1.classList.remove('animate-pulse')
     
-    intro.classList.add('opacity-0')
+//     intro.classList.add('opacity-0')
     
-    introH1.classList.add('opacity-0')  
+//     introH1.classList.add('opacity-0')  
     
-  }, 4000 ) 
-}
+//   }, 4000 ) 
+// }
 
-fadeOut()
-clearTimeout()
-sleep(6000).then(() => intro.classList.add('hidden'))
+// fadeOut()
+// clearTimeout()
+// sleep(6000).then(() => intro.classList.add('hidden'))
 
